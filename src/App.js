d0b745a4dfb19dc033a627e60696dc7b2794612c
Justin Miller <indymiller@gmail.com>
2020-06-24 15:47:18 -0400

    front end pulls all posts from backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://127.0.0.1:8000/api/posts/')
+    .then(res => res.json())
+    .then(data => this.setState({posts: data}))
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Ghostpost!</h1>
+        <ul>
+        {this.state.posts.map(p => {
+          return (
+            <div>
+            <li>Type: {p.post_type}</li>
+            <li>Content:  {p.content}</li>
+            <li>Likes: {p.up_vote}</li>
+            <li>Dislikes: {p.down_vote}</li>
+            <li>Dated: {p.sub_time}</li>
+            <br></br>
+            </div>)
+        })}
+        </ul>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
